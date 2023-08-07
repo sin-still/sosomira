ced1cf97f5209f18b40a2c83fbe9c7ce94932a5f
sinchul <kscpqk66@gmail.com>
2023-08-07 14:30:53 +0900

    서버경로 수정

--- src/config/constants.jsx
+++ src/config/constants.jsx
@@ -1,1 +1,1 @@
-export const API_URL = 'http://localhost:8080'
+export const API_URL=process.env.NODE_ENV === 'production'? " https://port-0-sosomira-server-ac2nll0any6w.sel4.cloudtype.app/" :"http://localhost:8080";  
