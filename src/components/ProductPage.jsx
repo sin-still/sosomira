6c67251736931511c3e5cc7b1c257e68f53443de
sinchul <kscpqk66@gmail.com>
2023-08-07 17:44:13 +0900

    상품등록 및 삭제하기버튼 비밀번호추가

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,96 +1,158 @@
 import { BiLoader } from "react-icons/bi";
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import "./ProductPage.scss";
 import { API_URL } from "../config/constants";
 import dayjs from "dayjs";
-import { Button, message } from 'antd';
+import { Button, message, Modal, Input } from "antd";
 
 const ProductPage = () => {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const [product, setProduct] = useState(null);
-    const getProduct = ()=>{
-        axios.get(`${ API_URL }/products/${id}`)
-            .then((result) => {
-                setProduct(result.data.product); // 데이터 구조에 맞게 .product를 추가합니다.
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [product, setProduct] = useState(null);
+  const getProduct = () => {
+    axios
+      .get(`${API_URL}/products/${id}`)
+      .then((result) => {
+        setProduct(result.data.product); // 데이터 구조에 맞게 .product를 추가합니다.
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        message.info("결제가 완료되었습니다.");
+      })
+      .catch((error) => {
+        message.error(`에러가 발생했습니다.${error.message}`);
+      });
+  };
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [passwordInput, setPasswordInput] = useState("");
+
+  const handlePasswordChange = (e) => {
+    setPasswordInput(e.target.value);
+  };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleModalOk = () => {
+    const correctPassword = "2684597"; // 실제 올바른 비밀번호로 대체해주세요
+    if (passwordInput === correctPassword) {
+      setIsModalVisible(false);
+      onDeleteProduct();
+    } else {
+      message.error("비밀번호가 올바르지 않습니다.");
     }
-    const onClickDelete = () => {
-        axios
-          .delete(`${API_URL}/products/${id}`)
-          .then((result) => {
-            message.info("상품을 삭제하였습니다.");
-            navigate(-1); // 삭제 후 이전 페이지로 이동
-          })
-          .catch((error) => {
-            message.error(`에러가 발생했습니다.${error.message}`);
-          });
-      };
+  };
 
-    useEffect(() => {
-        getProduct()
-    }, []);
+  const handleModalCancel = () => {
+    setIsModalVisible(false);
+  };
 
-    const onClickPurchase = ()=>{
-        axios.post(`${API_URL}/purchase/${id}`)
-        .then((result)=>{
+  const onClickDelete = () => {
+    showModal();
+  };
 
-            message.info('결제가 완료되었습니다.')
-            
+  const onDeleteProduct = () => {
+    axios
+      .delete(`${API_URL}/products/${id}`)
+      .then((result) => {
+        message.info("상품을 삭제하였습니다.");
+        navigate(-1); // 삭제 후 이전 페이지로 이동
+      })
+      .catch((error) => {
+        message.error(`에러가 발생했습니다.${error.message}`);
+      });
+  };
 
-        }).catch((error)=>{
+  console.log(product);
 
-            message.error(`에러가 발생했습니다.${error.message}`);
-            
-        });
-    }
-    
-    
-    console.log(product);
-
-    if (product === null) {
-        return <h2>상품정보를 받고 있습니다<BiLoader className="loadingIcon" /></h2>;
-    }
-
+  if (product === null) {
     return (
-        <div className="detailPage">
-            
-            <h2>상세페이지</h2>
-            <button onClick={() => navigate(-1)} id="back-btn">이전페이지</button>
-            <div className="detailPage-box">
-                {product.soldout === 1? <div className="product-blur"><span>SOLD OUT</span></div> : null}
-                <div id="image-box">
-                    <img src={`${API_URL}/${product.imageUrl}`} alt={product.name} />
-                </div>
-                <div id="profile-box">
-                    <img src="/img/products/avatar.png" alt={product.seller} />
-                    <span className="product-seller">{product.seller}</span>
-                </div>
-                <div id="contents-box" className="contents-box">
-                    <div id="name">{product.name}</div>
-                    <div id="price">{product.price}</div>
-                    <div id="createAt">{dayjs(product.createdAt).format('YYYY년 MM월 DD일')}</div>
-                    <Button type="primary" danger className="payment" size="large" onClick={onClickPurchase} disabled={product.soldout === 1? true : false}> 결제하기 </Button>
-                    <div id="description">{product.description}</div>
-    
-                </div>
-            </div>
-            <h3>{id}번째 페이지입니다.</h3>
-            <Button
-        type="primary"
-        danger
-        className="delete"
-        
-        onClick={onClickDelete}
-      >
-      </Button>
-        </div>
+      <h2>
+        상품정보를 받고 있습니다
+        <BiLoader className="loadingIcon" />
+      </h2>
     );
+  }
+
+  return (
+    <div className="detailPage">
+      <h2>상세페이지</h2>
+      <button onClick={() => navigate(-1)} id="back-btn">
+        이전페이지
+      </button>
+      <div className="detailPage-box">
+        {product.soldout === 1 ? (
+          <div className="product-blur">
+            <span>SOLD OUT</span>
+          </div>
+        ) : null}
+        <div id="image-box">
+          <img src={`${API_URL}/${product.imageUrl}`} alt={product.name} />
+        </div>
+        <div id="profile-box">
+          <img src="/img/products/avatar.png" alt={product.seller} />
+          <span className="product-seller">{product.seller}</span>
+        </div>
+        <div id="contents-box" className="contents-box">
+          <div id="name">{product.name}</div>
+          <div id="price">{product.price}</div>
+          <div id="createAt">
+            {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
+          </div>
+          <Button
+            type="primary"
+            danger
+            className="payment"
+            size="large"
+            onClick={onClickPurchase}
+            disabled={product.soldout === 1 ? true : false}
+          >
+            {" "}
+            결제하기{" "}
+          </Button>
+          <Button
+            type="primary"
+            danger
+            className="delete"
+            onClick={onClickDelete}
+            size="large"
+          >
+            삭제하기
+          </Button>
+          <Modal
+            title="비밀번호 입력"
+            visible={isModalVisible}
+            onOk={handleModalOk}
+            onCancel={handleModalCancel}
+          >
+            <Input
+              type="password"
+              value={passwordInput}
+              onChange={handlePasswordChange}
+              placeholder="비밀번호를 입력하세요."
+            />
+          </Modal>
+          <div id="description">{product.description}</div>
+          
+        </div>
+      </div>
+      <h3>{id}번째 페이지입니다.</h3>
+    </div>
+  );
 };
 
 export default ProductPage;
--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -1,84 +1,98 @@
 import { AiFillCamera } from "react-icons/ai";
 import React, { useState } from "react";
-import { Button, InputNumber, Divider, Form, Input, Upload, message } from "antd";
+import { Button, InputNumber, Divider, Form, Input, Upload, message, Modal } from "antd";
 import "./UploadPage.scss";
 import { API_URL } from "../config/constants";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-
-
 const { TextArea } = Input;
+
 const UploadPage = () => {
-  
-  const [ imageUrl, setImageUrl ] = useState(null)
+  const [imageUrl, setImageUrl] = useState(null);
+  const [isPasswordPopupVisible, setIsPasswordPopupVisible] = useState(false);
+  const [password, setPassword] = useState("");
   const history = useNavigate();
 
   const onSubmit = (values) => {
-    console.log("Success:", values);
-    axios.post(`${ API_URL }/products`,{
-      name: values.name,
-      description:values.description,
-      seller: values.seller,
-      price: parseInt(values.price), 
-      // parseInt: 정수로 가져오게끔 하는 것. 
-      imageUrl: imageUrl
-    }).then(( result )=>{
-      console.log(result);
-      history('/', {replace:true});
-    }).catch(( err )=>{
-      console.error(err);
-      message.error(`에러가 발생했습니다.${err.message}`)
-    })
+    // 비밀번호 팝업을 띄웁니다.
+    setIsPasswordPopupVisible(true);
   };
 
   const onChangeImage = (info) => {
-    if(info.file.status === 'uploading'){
-      return
+    if (info.file.status === "uploading") {
+      return;
     }
-    if(info.file.status === 'done'){
+    if (info.file.status === "done") {
       const response = info.file.response;
       const imageUrl = response.imageUrl;
-      setImageUrl(imageUrl)
+      setImageUrl(imageUrl);
     }
-  }
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handlePasswordSubmit = () => {
+    // 비밀번호 검증을 여기에서 수행합니다.
+    // 비밀번호가 올바르다면 상품 등록을 수행합니다.
+    if (password === "2684597") {
+      setIsPasswordPopupVisible(false); // 비밀번호가 올바른 경우, 팝업 닫기
+      axios
+        .post(`${API_URL}/products`, {
+          name: formValues.name,
+          description: formValues.description,
+          seller: formValues.seller,
+          price: parseInt(formValues.price),
+          imageUrl: imageUrl,
+        })
+        .then((result) => {
+          console.log(result);
+          history("/", { replace: true });
+        })
+        .catch((err) => {
+          console.error(err);
+          message.error(`에러가 발생했습니다.${err.message}`);
+        });
+    } else {
+      message.error("비밀번호가 올바르지 않습니다.");
+    }
+  };
+
+  const [form] = Form.useForm();
+  const formValues = form.getFieldsValue();
 
   return (
     <div id="upload-container">
-      <Form name="uploadForm" onFinish={onSubmit} initialValues={{ name: '', price: 0, seller: '', description: '' }}>
+      <Form form={form} name="uploadForm" onFinish={onSubmit} initialValues={{ name: "", price: 0, seller: "", description: "" }}>
         <Form.Item name="upload">
-          <Upload 
-          name="image" 
-          action={`${ API_URL}/image`} 
-          listType="picture" 
-          showUploadList={false} 
-          onChange={onChangeImage}>
-            {
-              imageUrl ? (<img id="upload-img" src={`${ API_URL }/${imageUrl}`} alt="img" />):(
+          <Upload
+            name="image"
+            action={`${API_URL}/image`}
+            listType="picture"
+            showUploadList={false}
+            onChange={onChangeImage}
+          >
+            {imageUrl ? (
+              <img id="upload-img" src={`${API_URL}/${imageUrl}`} alt="img" />
+            ) : (
               <div id="upload-img-placeholder">
                 <AiFillCamera className="icon-carmera" />
                 <span>이미지를 업로드해주세요</span>
-              </div>)
-            }
-            
+              </div>
+            )}
           </Upload>
         </Form.Item>
         <Divider />
         <Form.Item
           label={<span className="upload-label">상품명</span>}
           name="name"
-          rules={[
-            { required: true, message: "상품명은 필수 입력 사항입니다." },
-          ]}
+          rules={[{ required: true, message: "상품명은 필수 입력 사항입니다." }]}
         >
-          <Input
-            className="upload-name"
-            placeholder="상품명을 입력해주세요"
-            size="large"
-          />
+          <Input className="upload-name" placeholder="상품명을 입력해주세요" size="large" />
         </Form.Item>
         <Divider></Divider>
-
         <Form.Item
           label={<span className="upload-label">판매자명</span>}
           name="seller"
@@ -125,12 +139,16 @@ const UploadPage = () => {
           ></TextArea>
         </Form.Item>
         <Form.Item>
-          {/* {contextHolder} */}
-          <Button id="submit-button" htmlType="submit" /* onClick={info} */>
+          <Button id="submit-button" onClick={onSubmit}>
             상품등록하기
           </Button>
         </Form.Item>
       </Form>
+
+      {/* Password Popup */}
+      <Modal title="비밀번호 입력" visible={isPasswordPopupVisible} onOk={handlePasswordSubmit} onCancel={() => setIsPasswordPopupVisible(false)}>
+        <Input type="password" value={password} onChange={handlePasswordChange} placeholder="비밀번호를 입력해주세요" />
+      </Modal>
     </div>
   );
 };
